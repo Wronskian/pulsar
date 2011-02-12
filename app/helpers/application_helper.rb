d4bba20b761442e5cfbd5d135d753e1dda5a046f
Ryan Dalke <rdalke@gmail.com>
2011-02-12 03:38:52 -0700

    Some style and a left-sidebar helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,5 +12,14 @@ module ApplicationHelper
 	def logo
 		image_tag("logo.png", :alt => "Pulsar Logo")
 	end
+	
+	def left_sidebar
+		case
+			when @title == "Sign Up"
+				render "users/new-left-sidebar"
+			when @title == "Home"
+				render "pages/home-left-sidebar"
+		end
+	end
 
 end
